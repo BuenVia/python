c419e70c5bc52d57f3451a924278c8cc5307cb3d
Matt Clifford <matthewclifford@hotmail.co.uk>
2021-04-25 11:35:52 +0100

    updates

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-print('This is a string {}' .format('INSERTED'))
+# print('This is a string {}' .format('INSERTED'))
 
-y = 'NEW INSERT'
-x = 'This is a new string'
+# y = 'NEW INSERT'
+# x = 'This is a new string'
 
-print(f'{x} and {y}')
+# print(f'{x} and {y}')
+
+# NEW TASK
+
+my_list = [1,2,3]
+another_list = [4,5]
+
+print(my_list + another_list[1])
