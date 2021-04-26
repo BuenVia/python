6fa746f9d3f291ac5fc9c196aa7e69a39c84347c
Matt Clifford <matthewclifford@hotmail.co.uk>
2021-04-26 19:08:20 +0100

    today's update

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 # y = 'NEW INSERT'
 # x = 'This is a new string'
 
-# print(f'{x} and {y}')
+# # print(f'{x} and {y}')
 
-# NEW TASK
+# # NEW TASK
 
-my_list = [1,2,3]
-another_list = [4,5]
+# my_list = [1,2,3]
+# another_list = [4,5]
 
-print(my_list[1:])
+# print(my_list[1:])
 
-  
+my_dict = {'key1':'value1', 'key2':'value2', 'key3':'value3'}
+print(my_dict['key1'])
