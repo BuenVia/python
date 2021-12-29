ce6d939462afe81e0e9bf8ff9cb993eee440a021
Matt Clifford <matthewclifford@hotmail.co.uk>
2021-12-29 20:56:27 +0000

    new

---
+++ my_python_work.py
@@ -0,0 +1,11 @@
+#1. Numbers
+#Integers (whole numbers)
+print (1 + 1)
+#Floating Point Numbers
+print(2.3 + 9.8)
+
+#Print Formatting
+r = "rules"
+b = "Javascript is better"
+
+print(f"Python {r}... but {b}!")
