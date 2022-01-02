196329aeb8354d0513d7e232a2175d385982db89
Matt Clifford <matthewclifford@hotmail.co.uk>
2022-01-02 18:44:46 +0000

    update

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-side1 = int(input())
-side2 = int(input())
-side2 = int(input())
+n = 20
 
+for x in range(1, n):
+    if x % 3 == 0 and x % 5 == 0:
+        print("SoloLearn")
+    elif x % 3 == 0:
+        print("Solo")
+    elif x % 5 == 0:
+        print("Learn")
+    else:
+        print(x)
