92f93dae4e5a3d2fd2a63a2893014312ba046c64
Matt Clifford <matthewclifford@hotmail.co.uk>
2022-01-03 20:55:15 +0000

    update

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-n = 20
+n = int(input())
 
-for x in range(1, n):
-    if x % 3 == 0 and x % 5 == 0:
-        print("SoloLearn")
-    elif x % 3 == 0:
-        print("Solo")
-    elif x % 5 == 0:
-        print("Learn")
-    else:
-        print(x)
+length = 0
+
+while n > 0:
+    n //= 10
+    length =+ 1
+
+print(length)
