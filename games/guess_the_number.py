fb6f0f62ccedca093d95619ba05ecab1d49b0463
Matt Clifford <matthewclifford@hotmail.co.uk>
2022-01-09 16:33:00 +0000

    update

--- games/guess_the_number.py
+++ games/guess_the_number.py
@@ -1,3 +1,7 @@
 import random
-secretNumber = random.randint(1,20)
+secretNumber = random.randint(1, 20)
 print("I am thinking of a number between 1 and 20")
+
+for guessesTaken in range(1, 7):
+    print("Take a guess")
+    guess = int(input())
