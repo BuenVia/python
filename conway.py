8514324d7ce2efdaa399a4c3e012560638255ef9
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-01-11 15:52:05 +0000

    update

--- conway.py
+++ conway.py
@@ -2,3 +2,13 @@ import random, time, copy
 WIDTH = 60
 HEIGHT = 20
 
+nextCells = []
+
+for x in range(WIDTH):
+    column = []
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('#')
+        else:
+            column.append(" ")
+    nextCells.append(column)
