099a81bfeddaa5a708d611ae6a0804468184d5a7
Matthew Clifford <matthewclifford@hotmail.co.uk>
2022-01-11 16:14:24 +0000

    update

--- conway.py
+++ conway.py
@@ -28,3 +28,30 @@ while True:
             rightCoord = (x + 1) % WIDTH
             aboveCoord = (y - 1) % HEIGHT
             belowCoord = (y + 1) % HEIGHT
+
+            numNeighbours = 0
+            if currentCells[leftCoord][aboveCoord] == "#":
+                numNeighbours += 1
+            if currentCells[x][aboveCoord] == "#":
+                numNeighbours += 1
+            if currentCells[rightCoord][aboveCoord] == "#":
+                numNeighbours += 1
+            if currentCells[leftCoord][y] == "#":
+                numNeighbours += 1
+            if currentCells[rightCoord][y] == "#":
+                numNeighbours += 1
+            if currentCells[leftCoord][belowCoord] == "#":
+                numNeighbours += 1
+            if currentCells[x][belowCoord] == "#":
+                numNeighbours += 1
+            if currentCells[rightCoord][belowCoord] == "#":
+                numNeighbours += 1
+
+            if currentCells[x][y] == "#" and (numNeighbours == 2 or numNeighbours == 3):
+                nextCells[x][y] = "#"
+            elif currentCells[x][y] == " " and numNeighbours == 3:
+                nextCells[x][y] = "#"
+            else:
+                nextCells[x][y] = " "
+
+    time.sleep(1)
