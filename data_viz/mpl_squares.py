71f5824e75886dd5572ffd6534601de6765feff8
SaracensFan <matthewclifford@hotmail.co.uk>
2022-04-29 19:48:08 +0100

    update

--- data_viz/mpl_squares.py
+++ data_viz/mpl_squares.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+
+plt.style.use('fivethirtyeight')
+
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 #Set chart title and label axes
 
