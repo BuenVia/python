0dcdf6094456d5011daefb641fc37c68b63cdcf7
SaracensFan <matthewclifford@hotmail.co.uk>
2022-05-01 09:49:52 +0100

    update@

---
+++ data_viz/python.py
@@ -0,0 +1,30 @@
+# VARIABLES
+# new_variable = 'This is a variable'
+# print(new_variable)
+
+# STRING
+# new_string = 'This is a string'
+# #index
+# x = new_string[3]
+# print(x)
+# #slice
+# y = new_string[5:12]
+# z = new_string[-12: -4]
+# print(y, z)
+
+#NUMBERS
+#Int = 3
+#Float = 3.2
+
+#LISTS
+#Collection of items in order.
+list1 = ['String', 1, 5.3] #Ordered / Changable / 
+tuple1 = ('String', 1, 5.3)
+print(list1, tuple1)
+
+#FOR LOOP
+cars = ['ford', 'vauxhall', 'renault', 'seat', 'ferrari']
+for car in cars:
+    print(f"{car.title()} is a brand of car")
+
+print("The above is a list of car makes.")
