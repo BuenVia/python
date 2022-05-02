5d78a41210ee6464ceefb382c6dcf6496db3ddc3
SaracensFan <matthewclifford@hotmail.co.uk>
2022-05-02 13:43:05 +0100

    update

---
+++ data_viz/chapter_16/mapping_global_data_sets/data/eq_explore_data.py
@@ -0,0 +1,35 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+#Explore the structure of the data
+filename = 'python/data_viz/chapter_16/mapping_global_data_sets/data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+# Map the earthquake
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
+
+# print(mags[:10])
+# print(lons[:5])
+# print(lats[:5])
+
+# readable_file = 'python/data_viz/readable_eq_data.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
