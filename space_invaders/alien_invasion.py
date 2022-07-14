162c047edd6d4b5b486329c6e12b8aa669554ba5
Matt Clifford <matthewclifford@hotmail.co.uk>
2022-07-14 21:50:14 +0100

    update to modern version

--- space_invaders/alien_invasion.py
+++ space_invaders/alien_invasion.py
@@ -1,28 +1,47 @@
+import sys
+
 import pygame
 
 from settings import Settings
 from ship import Ship
-import game_functions as gf
 
-def run_game():
-    # Initialize pygame, settings and create a screen object
-    pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption('Alien Invasion')
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
 
-    # Make a ship
-    ship = Ship(ai_settings, screen)
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
+        self.settings = Settings()
 
-    # Set the background color
-    bg_color = (230, 230, 230)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
 
-    # Start the main loop for the game
-    while True:
+        self.ship = Ship(self)
 
-        gf.check_events(ship)
-        ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            # Watch for keyboard and mouse events
+            self._check_events()
+            self._update_screen()
 
-run_game()
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
--- space_invaders/ship.py
+++ space_invaders/ship.py
@@ -1,38 +1,18 @@
 import pygame
 
-class Ship():
+class Ship:
+    """A class to manage the ship"""
 
-    def __init__(self, ai_settings, screen):
-        """Initialize the ship and set its starting position."""
-        self.screen = screen
-        self.ai_settings = ai_settings
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
-        # Load the ship image and get its rect.
+        # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
-
-        # Start each new ship at the bottom center of the screen.
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
-
-        # Store a decimal value for the ship's center
-        self.center = float(self.rect.centerx)
-
-        # Movement flag
-        self.moving_right = False
-        self.moving_left = False
-
-    def update(self):
-        """Update the ship's postion based on the movement flags."""
-        # Update the ship's center value, not the rect
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed_factor
-
-        # Update rect object from self.center.
-        self.rect.centerx = self.center
+        # Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
         """Draw the ship at its current location"""
